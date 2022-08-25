ee9287746e76900eff46f101b1547d352b74db8f
Krzysztof Jamróz <kjamroz83@gmail.com>
2022-08-25 13:35:53 +0200

    fix: improve snail walk animation

--- src/SnailView.js
+++ src/SnailView.js
@@ -77,8 +77,14 @@ export default class SnailView extends Sprite {
 
     this.t = (this.t + 4) % 180;
 
-    this.body.scale.x = 0.9 + 0.1 *  Math.sin((this.t / 180) * Math.PI)
+
+
+    this.body.scale.x = 0.9 + (this.hidden ? 0 : 0.1) *  Math.sin((this.t / 180) * Math.PI)
     this.antiRotationContainer.rotation = -this.rotation;
+
+    this.shell.y = -0.07*Math.sin((this.t / 180) * Math.PI + 0.05)
+    this.eyeLeft.rotation = -0.4 * Math.sin((this.t / 180) * Math.PI)
+    this.eyeRight.rotation = -0.4 * Math.sin((this.t / 180) * Math.PI) - 0.5
   }
 
 
